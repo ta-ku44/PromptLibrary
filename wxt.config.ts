d6f6d1decfef0a03d87f1cb07235019d1f482583
ta-ku44 <213548904+ta-ku44@users.noreply.github.com>
2026-02-19 14:58:45 +0900

    fix: remove outDir configuration from wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  outDir: "dist", 
   modules: ['@wxt-dev/module-react'],
 });
